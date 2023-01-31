bc637fc7b210a23216fb6bc839b36a0fde14f7ac
thanhcorn <nnnt1012200@gmail.com>
2023-01-31 10:27:17 +0700

    Dựng khung Layout mặc định

--- src/pages/Following/index.js
+++ src/pages/Following/index.js
@@ -1,5 +1,5 @@
-function Home() {
-    return <h2>Home Page</h2>
+function Following() {
+    return <h2>Following Pagedsadfas fsadfsad</h2>
 }
 
-export default Home;
+export default Following;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
-function Following() {
-    return <h2>Following Page</h2>
+function Home() {
+    return <h2>Home Page</h2>;
 }
 
-export default Following;
+export default Home;
